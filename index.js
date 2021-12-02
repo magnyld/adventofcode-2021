38afb9cf0814b739d5e2c16f7a43a2b80e5b06e7
Andreas Jorneus <andreas@weightpoint.se>
2021-12-02 08:39:45 +0000

    Day2

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
 const express = require("express");
-const day1_data = require("./day1");
+//const currentDay = require("./day1");
+const currentDay = require("./day2");
 const app = express();
 const port = 3000;
 
-
-function getDay1_part2_data() {
-
-  var newData = [];
-
-  for (var j = 0; j <= day1_data.depths.length; j++) {
-    newData.push(day1_data.depths[j] + day1_data.depths[j+1] + day1_data.depths[j+3]);
-  }
-
-  return newData;
-}
-
-
-function calcIncreases (data) {
-
-  var inc = 0,
-      debug = "";
-
-  for (var j = 1; j <= data.length; j++) {
-
-    if (data[j-1] < data[j]) {
-      inc++;
-    }
-  }
-
-  return "increases: " + inc + debug;
-}
-
-function getResults() {
-
-  return "" +
-    "Day 1 part 1: " + calcIncreases(day1_data.depths) + "<br>" +
-    "Day 1 part 2: " + calcIncreases(getDay1_part2_data());
-
-}
-
-
-app.get("/", (req, res) => res.send(getResults()));
+app.get("/", (req, res) => res.send(currentDay.getResults()));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
